7ab535ebd15785d68d2aeac076cd7eab8a230177
ndminh1307js <ndminh1307js@gmail.com>
2020-06-07 22:51:44 +0700

    Loading data store

---
+++ src/ToggleLink.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+export class ToggleLink extends Component {
+  render() {
+    return (
+      <Route
+        path={this.props.to}
+        exact={this.props.exact}
+        children={(routeProps) => {
+          const baseClasses = this.props.className || 'm-2 btn btn-block';
+          const activeClass = this.props.activeClass || 'btn-primary';
+          const inactiveClass = this.props.inactiveClass || 'btn-secondary';
+          const combinedClass = `${baseClasses} ${
+            routeProps.match ? activeClass : inactiveClass
+          }`;
+
+          return (
+            <Link to={this.props.to} className={combinedClass}>
+              {this.props.children}
+            </Link>
+          );
+        }}
+      />
+    );
+  }
+}
